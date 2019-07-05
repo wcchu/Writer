0990fc41b8edff4f1577002592487784aec7a9fa
Wei-Chun <chu@fitanalytics.com>
2019-07-05 11:21:40 +0200

    update env

--- PairShift/venv.sh
+++ PairShift/venv.sh
@@ -5,7 +5,7 @@ virtualenv $prefix
 source $prefix/bin/activate
 easy_install -U pip
 pip install oauth2client httplib2 numpy protobuf pytest sklearn scipy pandas matplotlib cython pyreadr flake8 yapf
-pip install tensorflow==1.14.0
+pip install tensorflow==2.0.0-beta1
 # integrate with jupyter
 pip install ipykernel
 pip install library
