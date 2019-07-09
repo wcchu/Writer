4d1989616eb5e5aaa26b29423799c31a8b6483e3
Wei-Chun <chu@fitanalytics.com>
2019-07-09 16:38:24 +0200

    update tf version

--- PairLinCorr/venv.sh
+++ PairLinCorr/venv.sh
@@ -7,7 +7,7 @@ easy_install -U pip
 pip install --upgrade setuptools
 pip install --upgrade cython
 pip install oauth2client httplib2 numpy protobuf pytest sklearn scipy pandas matplotlib cython pyreadr flake8 yapf
-pip install tensorflow==1.14.0
+pip install tensorflow==2.0.0-beta1
 # integrate with jupyter
 pip install ipykernel
 pip install library
