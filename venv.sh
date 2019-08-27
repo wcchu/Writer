3315e3c7003e01155c1fdc3598c45f116bc027cc
Wei-Chun <chu@fitanalytics.com>
2019-08-27 10:53:13 +0200

    clean install packages

--- venv.sh
+++ venv.sh
@@ -3,12 +3,16 @@ prefix=env
 rm -r $prefix
 virtualenv --python=/usr/bin/python3 --system-site-packages $prefix
 source $prefix/bin/activate
-easy_install -U pip
-pip install --upgrade setuptools
-pip install --upgrade cython
-pip install oauth2client httplib2 numpy protobuf pytest sklearn scipy pandas matplotlib cython pyreadr flake8 yapf
-pip install tensorflow==2.0.0-beta1
-# integrate with jupyter
-pip install ipykernel
-pip install library
-pip install jupyter
+pip install --ignore-installed --no-cache-dir --upgrade \
+        pip \
+        numpy \
+        pandas \
+        sklearn \
+        scipy \
+        matplotlib \
+        flake8 \
+        yapf \
+        cython \
+        testresources \
+        jupyter \
+        tensorflow==2.0.0-beta1
