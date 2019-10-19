0b3f3a2015b120ae65e46c51794287659da63597
Wei-Chun Chu <weichun.chu@gmail.com>
2019-10-19 19:18:04 +0200

    use requirements.txt

--- venv.sh
+++ venv.sh
@@ -3,17 +3,4 @@ prefix=env
 rm -r $prefix
 virtualenv --python=/usr/bin/python3 --system-site-packages $prefix
 source $prefix/bin/activate
-pip install --ignore-installed --no-cache-dir --upgrade \
-        pip \
-        numpy \
-        pandas \
-        sklearn \
-        scipy \
-        matplotlib \
-        flake8 \
-        yapf \
-        cython \
-        testresources \
-        jupyter \
-        tensorflow \
-	Flask
+pip install --ignore-installed --no-cache-dir --upgrade -r requirements.txt
