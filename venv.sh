14c42f54cad65eea5484a74332318f438f2b2e8c
Wei-Chun Chu <weichun.chu@gmail.com>
2019-10-19 17:28:46 +0200

    tensorflow 2.0 as default

--- venv.sh
+++ venv.sh
@@ -15,5 +15,5 @@ pip install --ignore-installed --no-cache-dir --upgrade \
         cython \
         testresources \
         jupyter \
-        tensorflow==2.0.0-rc1 \
+        tensorflow \
 	Flask
