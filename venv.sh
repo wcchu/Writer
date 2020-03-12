aaec5799b7204605c1a5d0c0f9e470052c18236d
Wei-Chun <chu@fitanalytics.com>
2020-03-12 16:45:40 +0100

    update pip packages

--- venv.sh
+++ venv.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 prefix=env
 rm -r $prefix
-virtualenv --python=/usr/bin/python3 --system-site-packages $prefix
+virtualenv --python=/usr/bin/python3 $prefix
 source $prefix/bin/activate
 pip install --ignore-installed --no-cache-dir --upgrade -r requirements.txt
