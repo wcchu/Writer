09a61409b8312ff384f69733b462aef5300e011f
wcchu <weichun.chu@gmail.com>
2021-01-07 23:45:01 +0100

    install google cloud sdk and set up gcloud in train.sh

--- train.sh
+++ train.sh
@@ -2,6 +2,13 @@
 
 now=$(date +%s)
 
+# set up gcloud
+export PATH=/usr/local/google-cloud-sdk/bin:$PATH
+. .bashrc
+gcloud config set core/disable_usage_reporting true
+gcloud config set component_manager/disable_update_check true
+gcloud config set project fitanalytics-ds-dev
+
 # train model
 echo "training model..."
 python learn.py
