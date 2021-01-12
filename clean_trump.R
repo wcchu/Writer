6f9abe475b535720ce5922fadd48d15f81870097
weichunchu <chu@fitanalytics.com>
2021-01-12 17:11:08 +0100

    have single new line after each tweet

--- clean_trump.R
+++ clean_trump.R
@@ -1,10 +1,10 @@
-library(tidyverse)
+suppressPackageStartupMessages(library(tidyverse))
 
 d <- read.csv("trump_tweets.csv", stringsAsFactors = F)
 t <- d$text
 
-# join tweets to one string
-t <- paste(t, collapse = "")
+# join tweets to one string with new lines
+t <- paste(t, collapse = "\n")
 
 # remove emojis
 t <- gsub("[^\x01-\x7F]", "", t)
@@ -14,6 +14,9 @@ t <- str_remove_all(t, "@\\w*:")
 
 # change double white spaces to single white spaces
 t <- gsub("  ", " ", t)
+
+# change double new lines to single new lines
+t <- gsub("\n\n", "\n", t)
 
 # output
 write(t, "trump.txt")
