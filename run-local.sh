bc30ac9d68768de4debdb8e6f4bcfa22bb24e054
weichunchu <chu@fitanalytics.com>
2021-01-12 11:05:54 +0100

    include app engine deployment in run-local.sh

--- run-local.sh
+++ run-local.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
 
+# set up gcloud
+export PATH=/usr/local/google-cloud-sdk/bin:$PATH
+. .bashrc
+gcloud config set core/disable_usage_reporting true
+gcloud config set component_manager/disable_update_check true
+gcloud config set project writer-01
+gcloud auth activate-service-account --key-file=writer-app-engine.json
+
 # training
 echo "training model..."
 python learn.py
 
-# deployment
-echo "deploying model locally..."
+# deploy to google app engine
+echo "deploying service to app engine..."
+gcloud app deploy
+
+# deploy to local
+echo "deploying service locally..."
 python main.py
 
 echo "done"
