48e44870a847167e88adfe71e369b324ebe06638
wcchu <weichun.chu@gmail.com>
2021-05-28 14:04:36 +0200

    combine run-cloud.sh and run-local.sh to docker.sh

--- docker.sh
+++ docker.sh
@@ -1,23 +1,30 @@
 #!/usr/bin/env bash
 
+now=$(date +"%F-%T")
+
 # set up gcloud
 export PATH=/usr/local/google-cloud-sdk/bin:$PATH
 . .bashrc
 gcloud config set core/disable_usage_reporting true
 gcloud config set component_manager/disable_update_check true
 gcloud config set project writer-01
-gcloud auth activate-service-account --key-file=writer-app-engine.json
 
-# training
+# if running locally, download and use service account key:
+# gcloud auth activate-service-account --key-file=writer-app-engine.json
+
+# train model
 echo "training model..."
 python learn.py
 
+# upload model
+echo "uploading model..."
+gsutil cp *.pk gs://writer-training/${now}/
+gsutil cp -r checkpoints gs://writer-training/${now}/
+
 # deploy to google app engine
-echo "deploying service to app engine..."
 gcloud app deploy
 
-# deploy to local
-echo "deploying service locally..."
-python main.py
+# if deploying locally:
+# python main.py
 
 echo "done"
